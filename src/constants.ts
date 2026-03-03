9d6c9490247e01ee69555c25355d1c25d118fbba
SamuelPG2004 <pgonzalez30072004@gmail.com>
2026-03-02 20:48:49 -0400

    feat: add Venezuela to countries list

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const COUNTRIES = [
-  'Argentina', 'Brasil', 'Chile', 'Colombia', 'España', 'México', 'Perú', 'Uruguay', 'USA', 'Other'
+  'Argentina', 'Brasil', 'Chile', 'Colombia', 'España', 'México', 'Perú', 'Uruguay', 'USA', 'Venezuela', 'Other'
 ];
